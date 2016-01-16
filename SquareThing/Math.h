b64eabbc6a580e78dee333d851e136a0466b743b
Jonah Ring <armadilloring@gmail.com>
2016-01-16 10:55:53 -0800

    Adds Math model file

---
+++ SquareThing/Math.h
@@ -0,0 +1,13 @@
+//
+//  Math.h
+//  SquareThing
+//
+//  Created by Jonah Ring on 1/16/16.
+//  Copyright © 2016 Adam Goodwin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Math : NSObject
+
+@end
---
+++ SquareThing/Math.m
@@ -0,0 +1,13 @@
+//
+//  Math.m
+//  SquareThing
+//
+//  Created by Jonah Ring on 1/16/16.
+//  Copyright © 2016 Adam Goodwin. All rights reserved.
+//
+
+#import "Math.h"
+
+@implementation Math
+
+@end
